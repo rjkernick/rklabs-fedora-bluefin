aec7b7eb4696df07afabc2159ebd724f3edc46d6
Rob Kernick <rjkernick@gmail.com>
2026-09-24 07:26:35 -0400

    Build Bluefin DX with 1Password and Brave

--- Containerfile
+++ Containerfile
@@ -3,33 +3,15 @@ FROM scratch AS ctx
 COPY build_files /
 COPY system_files /system_files
 
-# Base Image
-FROM ghcr.io/ublue-os/bazzite:stable@sha256:9556db65991d57a03a7dc18e4ba28a686d8bcdcd6b61235aa69c8267bb22ff76
-## Other possible base images include:
-# FROM ghcr.io/ublue-os/bazzite:testing
-# FROM ghcr.io/ublue-os/aurora:stable
-# FROM ghcr.io/ublue-os/bluefin-nvidia-open:stable
-# 
-# ... and so on, here are more base images
-# Universal Blue Images: https://github.com/orgs/ublue-os/packages
-# Fedora base image: quay.io/fedora/fedora-bootc:44
-# CentOS base images: quay.io/centos-bootc/centos-bootc:stream10
+# Base Image: Bluefin DX (includes libvirt/virt-manager, Docker, Tailscale, distrobox, VS Code)
+FROM ghcr.io/ublue-os/bluefin-dx:stable
 
-### [IM]MUTABLE /opt
-## Some bootable images, like Fedora, have /opt symlinked to /var/opt, in order to
-## make it mutable/writable for users. However, some packages write files to this directory,
-## thus its contents might be wiped out when bootc deploys an image, making it troublesome for
-## some packages. Eg, google-chrome, docker-desktop.
-##
-## Uncomment the following line if one desires to make /opt immutable and be able to be used
-## by the package manager.
-
-# RUN rm /opt && mkdir /opt
+### IMMUTABLE /opt
+## 1Password and Brave install into /opt. On Fedora /opt is a symlink to /var/opt,
+## which is not carried over from the image, so make it a real directory in the image.
+RUN rm /opt && mkdir /opt
 
 ### MODIFICATIONS
-## make modifications desired in your image and install packages by modifying the build.sh script
-## the following RUN directive does all the things required to run "build.sh" as recommended.
-
 RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
     --mount=type=cache,dst=/var/cache \
     --mount=type=cache,dst=/var/log \
@@ -37,5 +19,4 @@ RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
     /ctx/build.sh
 
 ### LINTING
-## Verify final image and contents are correct.
 RUN bootc container lint
--- build_files/build.sh
+++ build_files/build.sh
@@ -5,23 +5,35 @@ set -ouex pipefail
 # Copy the contents of system_files/ of the git repo to /
 cp -avf "/ctx/system_files"/. /
 
-### Install packages
+### 1Password (desktop app + CLI)
+rpm --import https://downloads.1password.com/linux/keys/1password.asc
+cat > /etc/yum.repos.d/1password.repo <<'EOF'
+[1password]
+name=1Password Stable Channel
+baseurl=https://downloads.1password.com/linux/rpm/stable/$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://downloads.1password.com/linux/keys/1password.asc
+EOF
+dnf5 install -y 1password 1password-cli
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+# The RPM scriptlets create these groups in the image's /etc/group, but a deployed
+# system keeps its own /etc/group. Declare them via sysusers.d with the same GIDs
+# so they exist on every machine (needed for browser integration and `op`).
+for g in onepassword onepassword-cli; do
+    gid=$(getent group "$g" | cut -d: -f3)
+    echo "g $g $gid" > "/usr/lib/sysusers.d/$g.conf"
+done
 
-# this installs a package from fedora repos
-dnf5 install -y tmux
+### Brave (RPM, not Flatpak: 1Password browser integration doesn't work with Flatpak browsers)
+rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+curl -fsSLo /etc/yum.repos.d/brave-browser.repo \
+    https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+dnf5 install -y brave-browser
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+### Drop third-party repos: updates arrive through image rebuilds, not on the client
+rm -f /etc/yum.repos.d/1password.repo /etc/yum.repos.d/brave-browser.repo
 
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+### Services
+systemctl enable tailscaled.service
